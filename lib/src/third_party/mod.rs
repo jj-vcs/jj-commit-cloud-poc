fda89f8754198804e23726fd0cb03d802d396dee
Sid Rachabathuni <srachaba@google.com>
2026-07-07 10:13:44 +0000

    third_party: import protobuf serialization helpers from upstream jj
    Move proto_helpers.rs into a dedicated third_party module to cleanly separate
    imported Apache 2.0 upstream Jujutsu code from Google LLC copyright headers.
    Preserves original copyright header and attribution.

--- lib/src/third_party/mod.rs
+++ lib/src/third_party/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2024-2026 Google LLC
+// Copyright 2020 The Jujutsu Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod local_backend;
-pub mod local_op_store;
-pub mod third_party;
-
-
-
+pub mod proto_helpers;
