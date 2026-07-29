2ab858267d83c97345ae96acfb2b1e5de3393ca5
Sid Rachabathuni <srachaba@google.com>
2026-07-29 00:54:44 +0000

    tests: add failing integration test for 'jj log'
    Test for the functionality of the 'jj log' command after the initial
    repository initialization. This tests that a repository is able to be
    created, and when 'jj log' function is run immediately, the root commit
    exists and the log is fetched from the server and shown in the console.

---
+++ cli/tests/test_log_command.rs
@@ -0,0 +1,42 @@
+use tempfile::tempdir;
+
+#[tokio::test]
+#[should_panic(expected = "unrecognized subcommand")]
+async fn test_log_command_integration() {
+    // Spawn jj-cc-server using the shared dynamic port test harness
+    let server = testutils::spawn_server().await;
+
+    // Execute jj cc init inside a temporary test workspace
+    let temp_dir = tempdir().expect("temporary directory should have been created for testing");
+    let repo_path = temp_dir.path();
+
+    let mut init_cmd = assert_cmd::Command::cargo_bin("jj")
+        .expect("The jj CLI binary should have compiled");
+
+    init_cmd
+        .current_dir(repo_path)
+        .args([
+            "cc",
+            "init",
+            "--server",
+            server.url(),
+            "--create",
+            ".",
+        ]);
+
+    init_cmd.assert().success();
+
+    // Now run jj log for root() using -T commit_id
+    let mut log_cmd = assert_cmd::Command::cargo_bin("jj")
+        .expect("The jj CLI binary should have compiled");
+
+    log_cmd
+        .current_dir(repo_path)
+        .args(["log", "-r", "root()", "-T", "commit_id"]);
+
+    // Special character is needed to assert accurate log output. see cli/tests/test_log_command.rs upstream
+    log_cmd
+        .assert()
+        .success()
+        .stdout("◆  0000000000000000000000000000000000000000\n");
+}
