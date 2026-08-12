0cfad9e4f6ca837e466cafacfef0aa496a0d9ac2
Sid Rachabathuni <srachaba@google.com>
2026-08-12 23:22:47 +0000

    server: add a new /store directory in server/src to hold the different storage backends later
    Decouple the backend storage layer (only hashmap at the moment) from
    backend.rs and create a new directory where the storage backends live,
    and import them. backend.rs now only implements the BackendService, and
    this pattern will be used for OpStoreService and OpHeadsStoreService
    which will be able to use memorystore.

---
+++ server/src/store/memorystore.rs
@@ -0,0 +1,22 @@
+use std::collections::{HashMap, HashSet};
+use std::sync::Mutex;
+use cc_common::backend::*;
+
+pub type RepoId = String;
+pub type CommitId = Vec<u8>;
+pub type TreeId = Vec<u8>;
+pub type FileId = Vec<u8>;
+
+#[derive(Debug, Default)]
+pub struct MemoryStore {
+    pub repos: Mutex<HashSet<RepoId>>,
+    pub commits: Mutex<HashMap<RepoId, HashMap<CommitId, Commit>>>,
+    pub trees: Mutex<HashMap<RepoId, HashMap<TreeId, Vec<TreeEntry>>>>,
+    pub files: Mutex<HashMap<RepoId, HashMap<FileId, Vec<u8>>>>,
+}
+
+impl MemoryStore {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
