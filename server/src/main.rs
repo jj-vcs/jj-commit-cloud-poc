0524e910729b61f7d109beff8eff6389ded54a11
Sid Rachabathuni <srachaba@google.com>
2026-08-12 23:04:06 +0000

    server: refactor src/server/main.rs to break up hashing and backend service functions into seperate files
    Create a new file hash_utils.rs to hold all of the utilities to hash git compatible commits, blobs and trees. This file will later hold hashing utilities for views and operations. Refactor main.rs into a new file backend.rs that holds the BackendService rpc service. This is in preparation to later add OpStoreService and OpHeadsStoreService in their respective files.
    
    Created a new helper function ensure_repo_registered_error in the server backend.rs file to avoid repeating the 'repository should have been registered before ...' error message. Added a comment explaining why mulitplying is needed when allocating a string for the git commit hex string.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,117 +1,15 @@
 use clap::Parser;
-use std::collections::{HashMap, HashSet};
 use std::net::SocketAddr;
-use std::sync::Mutex;
 use tonic::transport::Server;
 use tracing::{info, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use cc_common::backend::backend_service_server::{BackendService, BackendServiceServer};
-use cc_common::backend::*;
+use cc_common::backend::backend_service_server::BackendServiceServer;
 
-const EMPTY_STRING_PLACEHOLDER: &str = "JJ_EMPTY_STRING";
+mod backend;
+mod hash_utils;
 
-fn signature_to_git(sig: Option<&cc_common::backend::Signature>) -> gix::actor::Signature {
-    let (name, email) = match sig {
-        Some(s) => (
-            if s.name.is_empty() { EMPTY_STRING_PLACEHOLDER } else { &s.name },
-            if s.email.is_empty() { EMPTY_STRING_PLACEHOLDER } else { &s.email },
-        ),
-        None => (EMPTY_STRING_PLACEHOLDER, EMPTY_STRING_PLACEHOLDER),
-    };
-    let (secs, offset_mins) = sig
-        .and_then(|s| s.timestamp.as_ref())
-        .map_or((0, 0), |t| (t.millis_since_epoch / 1000, t.tz_offset));
-
-    gix::actor::Signature {
-        name: name.into(),
-        email: email.into(),
-        time: gix::date::Time::new(secs, offset_mins * 60),
-    }
-}
-
-/// Standalone black-box function that computes a Git commit hash using `gix` (Gitoxide).
-pub fn compute_git_commit_hash(commit: &cc_common::backend::Commit) -> Vec<u8> {
-    use gix::objs::WriteTo;
-
-    let tree_id = commit
-        .root_tree_id
-        .first()
-        .and_then(|id| gix::hash::ObjectId::try_from(id.as_slice()).ok())
-        .unwrap_or_else(|| gix::hash::ObjectId::empty_tree(gix::hash::Kind::Sha1));
-
-    // Exclude Jujutsu's root commit ID (Git root commits have 0 parents in Git representation)
-    let parents: Vec<gix::hash::ObjectId> = commit
-        .parent_commit_ids
-        .iter()
-        .filter(|id| id.as_slice() != cc_common::ROOT_COMMIT_ID_BYTES)
-        .filter_map(|id| gix::hash::ObjectId::try_from(id.as_slice()).ok())
-        .collect();
-
-    let mut extra_headers = Vec::new();
-    if !commit.change_id.is_empty() {
-        let mut rev_change_id = commit.change_id.clone();
-        rev_change_id.reverse();
-        let hex_str = rev_change_id.iter().map(|b| format!("{:02x}", b)).collect::<String>();
-        extra_headers.push(("change-id".into(), hex_str.into()));
-    }
-
-    let gix_commit = gix::objs::Commit {
-        tree: tree_id,
-        parents: parents.into(),
-        author: signature_to_git(commit.author.as_ref()),
-        committer: signature_to_git(commit.committer.as_ref()),
-        encoding: None,
-        extra_headers,
-        message: commit.description.as_bytes().into(),
-    };
-
-    let mut buf = Vec::new();
-    gix_commit.write_to(&mut buf).expect("gix commit should have serialized successfully");
-    let hash = gix::objs::compute_hash(gix::hash::Kind::Sha1, gix::objs::Kind::Commit, &buf);
-    hash.as_bytes().to_vec()
-}
-
-/// Standalone black-box function that computes a Git blob hash using `gix` (Gitoxide).
-pub fn compute_git_blob_hash(content: &[u8]) -> Vec<u8> {
-    let hash = gix::objs::compute_hash(gix::hash::Kind::Sha1, gix::objs::Kind::Blob, content);
-    hash.as_bytes().to_vec()
-}
-
-/// Standalone black-box function that computes a Git tree hash using `gix` (Gitoxide).
-pub fn compute_git_tree_hash(entries: &[cc_common::backend::TreeEntry]) -> Vec<u8> {
-    use gix::objs::WriteTo;
-
-    let mut gix_entries = Vec::new();
-    for entry in entries {
-        let (kind, id_bytes) = match entry.value.as_ref().and_then(|v| v.value.as_ref()) {
-            Some(cc_common::backend::tree_value::Value::File(f)) => {
-                let k = if f.executable { gix::objs::tree::EntryKind::BlobExecutable } else { gix::objs::tree::EntryKind::Blob };
-                (k, &f.id[..])
-            }
-            Some(cc_common::backend::tree_value::Value::TreeId(id)) => (gix::objs::tree::EntryKind::Tree, &id[..]),
-            Some(cc_common::backend::tree_value::Value::SymlinkId(id)) => (gix::objs::tree::EntryKind::Link, &id[..]),
-            _ => (gix::objs::tree::EntryKind::Blob, &cc_common::ROOT_COMMIT_ID_BYTES[..]),
-        };
-        if let Ok(oid) = gix::hash::ObjectId::try_from(id_bytes) {
-            gix_entries.push(gix::objs::tree::Entry {
-                mode: kind.into(),
-                filename: entry.name.as_bytes().into(),
-                oid,
-            });
-        }
-    }
-
-    // Sort entries according to Git canonical tree entry ordering rules (gix::objs::tree::Entry implements Ord for Git tree sorting).
-    gix_entries.sort_unstable();
-
-    let gix_tree = gix::objs::Tree { entries: gix_entries };
-    let mut buf = Vec::new();
-    gix_tree.write_to(&mut buf).expect("gix tree should have serialized successfully");
-
-    let hash = gix::objs::compute_hash(gix::hash::Kind::Sha1, gix::objs::Kind::Tree, &buf);
-    hash.as_bytes().to_vec()
-}
+pub use backend::CommitCloudServerImpl;
 
 #[derive(Parser, Debug)]
 #[command(name = "jj-cc-server", about = "Jujutsu Commit Cloud Server")]
@@ -125,201 +23,14 @@ struct Args {
     port: u16,
 }
 
-#[derive(Debug, Default)]
-pub struct CommitCloudServerImpl {
-    repos: Mutex<HashSet<String>>,
-    commits: Mutex<HashMap<String, HashMap<Vec<u8>, Commit>>>,
-    trees: Mutex<HashMap<String, HashMap<Vec<u8>, Vec<TreeEntry>>>>,
-    files: Mutex<HashMap<String, HashMap<Vec<u8>, Vec<u8>>>>,
-}
-
-#[tonic::async_trait]
-impl BackendService for CommitCloudServerImpl {
-    async fn register_repository(
-        &self,
-        request: tonic::Request<RegisterRepositoryRequest>,
-    ) -> Result<tonic::Response<RegisterRepositoryResponse>, tonic::Status> {
-        let req = request.into_inner();
-        info!("Registering repository: {}", req.repo_id);
-        let mut repos = self.repos.lock().unwrap();
-        repos.insert(req.repo_id.clone());
-        Ok(tonic::Response::new(RegisterRepositoryResponse {
-            repo_id: req.repo_id,
-        }))
-    }
-
-    async fn read_commit(
-        &self,
-        request: tonic::Request<ReadCommitRequest>,
-    ) -> Result<tonic::Response<ReadCommitResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-        let commit_id = req.commit_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before requesting commits"));
-        }
-
-        let commits = self.commits.lock().unwrap();
-        if let Some(repo_commits) = commits.get(&repo_id) {
-            if let Some(commit) = repo_commits.get(&commit_id) {
-                return Ok(tonic::Response::new(ReadCommitResponse {
-                    commit: Some(commit.clone()),
-                }));
-            }
-        }
-        Err(tonic::Status::not_found("commit should have been present in cloud database"))
-    }
-
-    async fn write_commit(
-        &self,
-        request: tonic::Request<WriteCommitRequest>,
-    ) -> Result<tonic::Response<WriteCommitResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before requesting commits"));
-        }
-
-        let mut commit = req.commit.ok_or_else(|| tonic::Status::invalid_argument("request should have contained commit data"))?;
-        let commit_id = if commit.commit_id.is_empty() {
-            compute_git_commit_hash(&commit)
-        } else {
-            commit.commit_id.clone()
-        };
-        commit.commit_id = commit_id.clone();
-        info!("Writing commit {:?} for repo {}", commit_id, repo_id);
-
-        let mut commits = self.commits.lock().unwrap();
-        let repo_commits = commits.entry(repo_id).or_default();
-        repo_commits.insert(commit_id.clone(), commit);
-
-        Ok(tonic::Response::new(WriteCommitResponse { commit_id }))
-    }
-
-    async fn read_tree(
-        &self,
-        request: tonic::Request<ReadTreeRequest>,
-    ) -> Result<tonic::Response<ReadTreeResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-        let tree_id = req.tree_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before requesting trees"));
-        }
-
-        if tree_id == cc_common::EMPTY_TREE_ID_BYTES {
-            return Ok(tonic::Response::new(ReadTreeResponse {
-                tree_id,
-                entries: vec![],
-            }));
-        }
-
-        let trees = self.trees.lock().unwrap();
-        if let Some(repo_trees) = trees.get(&repo_id) {
-            if let Some(entries) = repo_trees.get(&tree_id) {
-                return Ok(tonic::Response::new(ReadTreeResponse {
-                    tree_id,
-                    entries: entries.clone(),
-                }));
-            }
-        }
-        Err(tonic::Status::not_found("tree should have been present in cloud database"))
-    }
-
-    async fn write_tree(
-        &self,
-        request: tonic::Request<WriteTreeRequest>,
-    ) -> Result<tonic::Response<WriteTreeResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before writing trees"));
-        }
-
-        let tree_id = compute_git_tree_hash(&req.entries);
-
-        let mut trees = self.trees.lock().unwrap();
-        let repo_trees = trees.entry(repo_id).or_default();
-        repo_trees.insert(tree_id.clone(), req.entries);
-
-        Ok(tonic::Response::new(WriteTreeResponse { tree_id }))
-    }
-
-    type ReadFileStream = tokio_stream::wrappers::ReceiverStream<Result<ReadFileResponse, tonic::Status>>;
-
-    async fn read_file(
-        &self,
-        request: tonic::Request<ReadFileRequest>,
-    ) -> Result<tonic::Response<Self::ReadFileStream>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-        let file_id = req.file_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before reading files"));
-        }
-
-        let files = self.files.lock().unwrap();
-        let content = files
-            .get(&repo_id)
-            .and_then(|repo_files| repo_files.get(&file_id))
-            .cloned()
-            .ok_or_else(|| tonic::Status::not_found("file should have been present in cloud database"))?;
-
-        let (tx, rx) = tokio::sync::mpsc::channel(1);
-        tokio::spawn(async move {
-            let _ = tx.send(Ok(ReadFileResponse { chunk: content })).await;
-        });
-
-        Ok(tonic::Response::new(tokio_stream::wrappers::ReceiverStream::new(rx)))
-    }
-
-    // TODO: Upgrade write_file RPC handler to consume tonic::Streaming<WriteFileRequest>
-    // to handle chunked streaming uploads for large files (>4MB) without hitting gRPC limits.
-    async fn write_file(
-        &self,
-        request: tonic::Request<WriteFileRequest>,
-    ) -> Result<tonic::Response<WriteFileResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let repo_id = req.repo_id;
-
-        if !self.repos.lock().unwrap().contains(&repo_id) {
-            return Err(tonic::Status::not_found("repository should have been registered before writing files"));
-        }
-
-        let file_id = compute_git_blob_hash(&req.content);
-
-        let mut files = self.files.lock().unwrap();
-        let repo_files = files.entry(repo_id).or_default();
-        repo_files.insert(file_id.clone(), req.content);
-
-        Ok(tonic::Response::new(WriteFileResponse { file_id }))
-    }
-
-    async fn read_symlink(
-        &self,
-        _request: tonic::Request<ReadSymlinkRequest>,
-    ) -> Result<tonic::Response<ReadSymlinkResponse>, tonic::Status> {
-        Err(tonic::Status::unimplemented("Not implemented yet"))
-    }
-
-    async fn write_symlink(
-        &self,
-        _request: tonic::Request<WriteSymlinkRequest>,
-    ) -> Result<tonic::Response<WriteSymlinkResponse>, tonic::Status> {
-        Err(tonic::Status::unimplemented("Not implemented yet"))
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::EnvFilter::from_default_env().add_directive("jj_cc_server=info".parse()?))
+        .with(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("jj_cc_server=info".parse()?),
+        )
         .init();
 
     let args = Args::parse();
@@ -346,7 +57,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     info!("Shutdown signal received, shutting down gracefully...");
                 }
                 Err(e) => {
-                    warn!("Failed to listen for shutdown signal: {}, shutting down...", e);
+                    warn!(
+                        "Failed to listen for shutdown signal: {}, shutting down...",
+                        e
+                    );
                 }
             }
         })
