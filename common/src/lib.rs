d74e10fee25516c0b093d923dca5cf8db0014e00
Sid Rachabathuni <srachaba@google.com>
2026-08-12 19:37:54 +0000

    tests: add failing integration test for 'jj op log' and test verifying commit_cloud op store configuration
    lib: add CommitCloudOpStore and CommitCloudOpHeadsStore trait stub implementations and register them in StoreFactoriesExt so they can be used with the jj-cli
    
    common: add shared constants for operation id and view id lengths
    
    Test for the functionality of the 'jj op log' command. This test verifies that the operation log contains the init repository operation with the correct output, and contains the describe and 'jj log' operations after a modification to the working directory. Also verifies that 'jj cc init' configures the repository op_store and op_heads_store type files as 'commit_cloud'. We need to initialize in this commit to make the test use our commit cloud op store instead of defaulting to the local op store. Adding shared constants to common in this commit is needed to avoid hardcoding commit id length in load() function. Change init_with_backend to init_with_factories so that our commit cloud op store and op heads store can also be initialized.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,8 +4,12 @@ pub mod backend {
 
 pub const COMMIT_ID_LENGTH: usize = 20;
 pub const CHANGE_ID_LENGTH: usize = 16;
+pub const OPERATION_ID_LENGTH: usize = 16;
+pub const VIEW_ID_LENGTH: usize = 16;
 pub const ROOT_COMMIT_ID_BYTES: [u8; COMMIT_ID_LENGTH] = [0u8; COMMIT_ID_LENGTH];
 pub const ROOT_CHANGE_ID_BYTES: [u8; CHANGE_ID_LENGTH] = [0u8; CHANGE_ID_LENGTH];
+pub const ROOT_OPERATION_ID_BYTES: [u8; OPERATION_ID_LENGTH] = [0u8; OPERATION_ID_LENGTH];
+pub const ROOT_VIEW_ID_BYTES: [u8; VIEW_ID_LENGTH] = [0u8; VIEW_ID_LENGTH];
 pub const EMPTY_TREE_ID_HEX: &str = "4b825dc642cb6eb9a060e54bf8d69288fbee4904";
 pub const EMPTY_TREE_ID_BYTES: [u8; 20] = [
     0x4b, 0x82, 0x5d, 0xc6, 0x42, 0xcb, 0x6e, 0xb9, 0xa0, 0x60, 0xe5,
---
+++ lib/src/cc_op_heads_store.rs
@@ -0,0 +1,53 @@
+use async_trait::async_trait;
+use jj_lib::op_heads_store::{OpHeadsStore, OpHeadsStoreError, OpHeadsStoreLock};
+use jj_lib::op_store::OperationId;
+use std::fmt::Debug;
+use std::path::Path;
+
+use crate::util::CommitCloudConfig;
+
+#[derive(Debug)]
+pub struct CommitCloudOpHeadsStore {
+    server_url: String,
+    repo_id: String,
+}
+
+struct CommitCloudOpHeadsStoreNoLock;
+impl OpHeadsStoreLock for CommitCloudOpHeadsStoreNoLock {}
+
+impl CommitCloudOpHeadsStore {
+    pub fn name() -> &'static str {
+        "commit_cloud"
+    }
+
+    pub fn load(store_path: &Path) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        let config = CommitCloudConfig::load_from_store(store_path)?;
+        Ok(Self {
+            server_url: config.server_url,
+            repo_id: config.repo_id,
+        })
+    }
+}
+
+#[async_trait]
+impl OpHeadsStore for CommitCloudOpHeadsStore {
+    fn name(&self) -> &str {
+        Self::name()
+    }
+
+    async fn update_op_heads(
+        &self,
+        _old_ids: &[OperationId],
+        _new_id: &OperationId,
+    ) -> Result<(), OpHeadsStoreError> {
+        unimplemented!()
+    }
+
+    async fn get_op_heads(&self) -> Result<Vec<OperationId>, OpHeadsStoreError> {
+        unimplemented!()
+    }
+
+    async fn lock(&self) -> Result<Box<dyn OpHeadsStoreLock + '_>, OpHeadsStoreError> {
+        Ok(Box::new(CommitCloudOpHeadsStoreNoLock))
+    }
+}
